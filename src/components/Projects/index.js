7fcc21baac090b8e708dfdb475d5a9069a3155ba
andreapdev <andreap.dev@gmail.com>
2021-04-26 16:15:33 +0200

    projects added

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,27 +1,41 @@
 import React from 'react';
 import Section from '../Section';
-import StyledArticle from './style';
+import StyledSection from './style';
+
+//Assets
+import mySalesPic from '../../assets/mysales.png';
+import libreriaPic from '../../assets/libreria.png';
+import cartPic1 from '../../assets/checkout1.png';
+import cartPic2 from '../../assets/checkout2.png';
+
 
 const Projects = () => {
     return (
         <Section id="projects" title="Projects">
-            <StyledArticle>
-                <a href='https://github.com/andreapdev/my-sales'>
-                    <h4>'MySales' sales registry</h4>
-                </a>
-            </StyledArticle>
-            <StyledArticle>
-                <a href='https://github.com/andreapdev/ecommerce-exercici'>
-                    <h4>'La Librería' eCommerce</h4>
-                
-                </a>
+            <StyledSection>
+                <article className='small-container'> 
+                    <a href='https://github.com/andreapdev/my-sales'>
+                        <h4>'MySales' sales registry</h4>
+                        <img alt='web screenshot' src={mySalesPic} />
+                    </a>
+                </article>
+                <article className='small-container'>
+                    <a href='https://github.com/andreapdev/ecommerce-exercici'>
+                        <h4>'La Librería' eCommerce</h4>
+                        <img alt='web screenshot' src={libreriaPic} />
+                    </a>
 
-            </StyledArticle>
-            <StyledArticle>
-                <a href='https://github.com/andreapdev/checkout-app'>
-                    <h4>eCommerce Cart</h4>
-                </a>
-            </StyledArticle>
+                </article>
+                <article className='big-container'>
+                    <a href='https://github.com/andreapdev/checkout-app'>
+                        <h4>eCommerce Cart</h4>
+                        <div className='img-container'>
+                            <div><img alt='web screenshot' src={cartPic1} /></div>
+                            <div><img alt='web screenshot' src={cartPic2} /></div>
+                        </div>
+                    </a>
+                </article>
+            </StyledSection>
         </Section>
     );
 }
--- src/components/Projects/style.js
+++ src/components/Projects/style.js
@@ -1,8 +1,52 @@
 import styled from 'styled-components';
 import Colors from '../../theme/Colors';
 
-const StyledArticle = styled.article`
-    
+const StyledSection = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+
+    article{
+        margin: 0.5rem;
+        padding: 0.5rem;
+        border: solid 0.2rem ${Colors.warningColor};
+        box-shadow: 0.5rem 0.5rem 0 ${Colors.warningColor};
+        border-radius: 1rem;
+        text-align: center;
+        color: ${Colors.primaryColor};
+        font-size: 1.2rem;
+
+        img{
+            object-fit: cover;
+            width: 90%;
+            height: auto;
+        }
+        .big-container{
+            width: 100%;
+        }
+    }
+    @media only screen and (min-width: 768px) {
+        article:hover{
+            transform: translate(0.5rem, 0.5rem);
+            box-shadow: none;
+        }
+        .small-container{
+            width: 46%;
+        }
+        .img-container{
+            display: flex;
+            width: 100%;
+        }
+    }
+    @media only screen and (max-width: 767px) {
+        article{
+            width: 70%;
+        }
+        .img-container{
+            display: block;
+        }
+    }
 `;
 
-export default StyledArticle;
+export default StyledSection;
